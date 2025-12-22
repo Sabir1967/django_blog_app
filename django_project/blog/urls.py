d5f6438299d2e40e8435082272d40fde29f904a7
Sabir Shah <zeshanlook@gmail.com>
2025-12-22 14:18:22 +0500

    new changes 2

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,24 +1,3 @@
-'''
-# blog/urls.py
-
-from django.urls import path
-from .views import post_list, post_detail # new
-
-urlpatterns = [
-
-path("post/<int:pk>/", post_detail, name="post_detail"), # new
-path("", post_list, name="home"),
-
-]
-#blog/urls.py
-from django.urls import path
-from .views import BlogListView, BlogDetailView # new
-urlpatterns = [
-path("post/<int:pk>/", BlogDetailView.as_view(), name="post_detail"), # new
-path("", BlogListView.as_view(), name="home"), # new
-]
-
-'''
 # blog/urls.py
 from django.urls import path
 from .views import BlogListView, BlogDetailView, BlogCreateView # new
