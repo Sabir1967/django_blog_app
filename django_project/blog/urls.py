00bf0f1165654e2c68afa03d01164f177e584640
Sabir Shah <zeshanlook@gmail.com>
2025-12-22 14:01:39 +0500

    new changes

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,3 +1,4 @@
+'''
 # blog/urls.py
 
 from django.urls import path
@@ -8,4 +9,21 @@ urlpatterns = [
 path("post/<int:pk>/", post_detail, name="post_detail"), # new
 path("", post_list, name="home"),
 
+]
+#blog/urls.py
+from django.urls import path
+from .views import BlogListView, BlogDetailView # new
+urlpatterns = [
+path("post/<int:pk>/", BlogDetailView.as_view(), name="post_detail"), # new
+path("", BlogListView.as_view(), name="home"), # new
+]
+
+'''
+# blog/urls.py
+from django.urls import path
+from .views import BlogListView, BlogDetailView, BlogCreateView # new
+urlpatterns = [
+path("post/new/", BlogCreateView.as_view(), name="post_new"), # new
+path("post/<int:pk>/", BlogDetailView.as_view(), name="post_detail"),
+path("", BlogListView.as_view(), name="home"),
 ]
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,11 +1,17 @@
 # blog/views.py
-from django.shortcuts import render, get_object_or_404 # new
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView # new
 from .models import Post
 
-def post_list(request):
-    posts = Post.objects.all()
-    return render(request, 'home.html', {'posts': posts})
+class BlogListView(ListView):
+    model = Post
+    template_name = "home.html"
 
-def post_detail(request, pk): # new
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, "post_detail.html", {"post": post})
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = "post_detail.html"
+
+class BlogCreateView(CreateView): # new
+    model = Post
+    template_name = "post_new.html"
+    fields = ["title", "author", "body"]
